be9aec948bfce2b06281a2a239585b62aff939d7
sultonakobirov <zafarzhanovich@gmail.com>
2023-04-06 15:59:30 +0500

    "code optimizing"

--- js/script.js
+++ js/script.js
@@ -1,193 +1,107 @@
 let successful = []
-
 let unsuccessful = []
-
 let taxes = 0
-
 let taxesMax = {}
-
 let taxesMin = {}
 
-let allTaxes = []
-
-
-
-
-
 let bank = [
-{
-
-name: 'Apple',
-
-budget: 1000000,
-
-tax: 28,
-
-expensesPerYear: [
-
-{ title: 'Salaries', total: 125000 },
-
-{ title: 'Utilites', total: 18000, },
-
-{ title: 'Rent', total: 258000 }
-
+    {
+    name: 'Apple',
+    budget: 1000000,
+    tax: 28,
+    expensesPerYear: [
+        { 
+            title: 'Salaries', 
+            total: 125000 
+        },
+        { 
+            title: 'Utilites',
+            total: 18000, 
+        },
+        {  
+            title: 'Rent', 
+            total: 258000 
+        }
+        ]
+    },
+    {
+    name: 'Microsoft',
+    budget: 988000,
+    tax: 21,
+    expensesPerYear: [
+        {
+            title: 'Salaries',
+            total: 148000
+        },
+        {
+            title: 'Utilites',
+            total: 124000,
+        },
+        {
+            title: 'Rent',
+            total: 314000,
+        }
+        ]
+    },
+    {
+    name: 'HP',
+    budget: 609000,
+    tax: 14,
+    expensesPerYear: [
+        {
+            title: 'Salaries',
+            total: 414000
+        }, 
+        {
+            title: 'Utilites',
+            total: 19000,
+        },
+        {
+            title: 'Rent',
+            total: 114400
+        }
+    ]
+    },
+    {
+        name: 'Xiaomi',
+        budget: 889500,
+        tax: 17,
+        expensesPerYear: [
+            {
+                title: 'Salaries',
+                total: 318000                
+            },
+            {
+                title: 'Utilites',
+                total: 14000,
+            },
+            {
+                title: 'Rent',
+                total: 169000
+            }
+        ]
+    },
+    {
+        name: 'Samsung',
+        budget: 889500,
+        tax: 12,
+        expensesPerYear: [
+            {
+                title: 'Salaries',
+                total: 650400
+            },
+            {
+                title: 'Utilites',
+                total: 29000,
+            },
+            {
+                title: 'Rent',
+                total: 212000
+            }
+        ]
+    },
 ]
 
-},
-
-{
-
-name: 'Microsoft',
-
-budget: 988000,
-
-tax: 21,
-
-expensesPerYear: [
-
-{
-
-title: 'Salaries',
-
-total: 148000
-
-},
-
-{
-
-title: 'Utilites',
-
-total: 124000,
-
-},
-
-{
-
-title: 'Rent',
-
-total: 314000,
-
-}
-
-]
-
-},
-
-{
-
-name: 'HP',
-
-budget: 609000,
-
-tax: 14,
-
-expensesPerYear: [
-
-{
-
-title: 'Salaries',
-
-total: 414000
-
-}, 
-
-{
-
-title: 'Utilites',
-
-total: 19000,
-
-},
-
-{
-
-title: 'Rent',
-
-total: 114400
-
-}
-
-]
-
-},
-
-{
-
-name: 'Xiaomi',
-
-budget: 889500,
-
-tax: 17,
-
-expensesPerYear: [
-
-{
-
-title: 'Salaries',
-
-total: 318000
-
-},
-
-{
-
-title: 'Utilites',
-
-total: 14000,
-
-},
-
-{
-
-title: 'Rent',
-
-total: 169000
-
-}
-
-]
-
-},
-
-{
-
-name: 'Samsung',
-
-budget: 889500,
-
-tax: 12,
-
-expensesPerYear: [
-
-{
-
-title: 'Salaries',
-
-total: 650400
-
-},
-
-{
-
-title: 'Utilites',
-
-total: 29000,
-
-},
-
-{
-
-title: 'Rent',
-
-total: 212000
-
-}
-
-]
-
-},
-
-]
 let bankClone = JSON.parse(JSON.stringify(bank)) //cloning array
 
 const setup = (array) => {
@@ -210,15 +124,18 @@ const setup = (array) => {
             successful.push(element)
         }else{
             unsuccessful.push(element)
-        } 
+        } // сортируем на successful и unsuccessful
 
         //task4
         taxes += element.tax * element.budget / 100
+        //Слагаем все налоги элементов и записываем их в одну переменную
 
         //task6
         element.taxMoney = (element.tax * element.budget / 100)
-        taxesMax = array.reduce((prev, current) => prev.taxMoney > current.taxMoney ? prev : current, {});
+        taxesMax = array.reduce((prev, current) => prev.taxMoney > current.taxMoney ? prev : current, {});  
         taxesMin = array.reduce((prev, current) => prev.taxMoney < current.taxMoney ? prev : current, {});
+        //Находим максимальный и минимальный налог, оплачиваемый компаниями используя reduce() ссылка на решение этой задачи:https://qna.habr.com/q/609651
+
     })
     //outputs
     console.log(array);
@@ -227,9 +144,10 @@ const setup = (array) => {
     console.log("<Unsuccessful>");
     console.log(unsuccessful);
     console.log("<Taxes>");
-    console.log(taxes);
+    console.log(`Total Taxes: ${taxes}`);
     console.log("<TaxesMax>");
     console.log(taxesMax);
+    console.log("<TaxesMin>");
     console.log(taxesMin);
 }
 
